69116f96663bb11ba0595627be6eb58f82497706
Hosein <hs.ramezanpoor@gmail.com>
2018-11-09 13:41:36 +0330

    Add InstagramAccountTags class to find posts account tagged in

--- handlers.py
+++ handlers.py
@@ -3,14 +3,21 @@ from core import AbstractAccountHandler
 
 class InstagramAccountsList(AbstractAccountHandler):
     """Get all instagram business accounts ids for all pages of user whom your
-    providing his/her access_token in class initiation"""
+    providing his/her access_token in class initiation
+        inputs:
+            - access_token
+    """
 
     path = '/me/accounts/'
     fields = ['instagram_business_account']
 
 
 class InstagramAccountData(AbstractAccountHandler):
-    """Get account's data from facebook api for given instagram account id"""
+    """Get account's data from facebook api for given instagram account id
+        inputs:
+            - access_token
+            - instagram_business_account_id
+    """
     fields = (
         'username', 'name', 'biography', 'followers_count', 'follows_count',
         'ig_id', 'media_count', 'profile_picture_url', 'website',
@@ -23,10 +30,31 @@ class InstagramAccountData(AbstractAccountHandler):
 
 
 class InstagramAccountInsights(AbstractAccountHandler):
-    """To fetch instagram insight from account which account is provided"""
+    """To fetch instagram insight from account which account is provided
+        inputs:
+            - access_token
+            - instagram_business_account_id
+    """
     metrics = ['impressions', ' reach', ' profile_views']
     period = 'day'
 
     @property
     def path(self):
         return '{}/insights/'.format(self.instagram_business_account_id)
+
+
+class InstagramAccountTags(AbstractAccountHandler):
+    """Find Instagram posts which provided account tagged in
+        inputs:
+            - access_token
+            - instagram_business_account_id
+    """
+    fields = (
+        'caption', 'comments_count', 'username', 'id', 'like_count',
+        'media_type', 'media_url', 'owner', 'permalink', 'timestamp'
+    )
+    limit = 100
+
+    @property
+    def path(self):
+        return '{}/tags/'.format(self.instagram_business_account_id)
