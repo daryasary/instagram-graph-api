a8921f5ecc29aeaff4797189449acdf27aebad2f
Hosein <hs.ramezanpoor@gmail.com>
2019-01-08 17:49:20 +0330

    Add todo for replacing fid_package with package names list

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/daryasary/instagram-graph-api",
+    # todo: requests is not going to be detected
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
