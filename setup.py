eccdf5db087a47adf6bb45a33b9cd561830008e2
Hosein <hs.ramezanpoor@gmail.com>
2019-01-08 17:22:12 +0330

    Add setup.py file, and package 0.0.1 version

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="graph_api",
+    version="0.0.1",
+    author="daryasary",
+    author_email="hosein@inprobes.com",
+    description="Python sdk for Instagram graph api(facebook version)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/daryasary/instagram-graph-api",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Apache",
+    ],
+)
