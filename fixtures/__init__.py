97c90f52ba0c3ad6ce42f548c7c4145271b6aa72
Hosein <hs.ramezanpoor@gmail.com>
2019-01-19 12:49:36 +0330

    Add fixtures package to store credentials data

---
+++ fixtures/__init__.py
@@ -0,0 +1,4 @@
+try:
+    from fixtures.local_variables import *
+except ImportError:
+    from fixtures.default_variables import *
---
+++ fixtures/default_variables.py
@@ -0,0 +1,8 @@
+# Sample variables added here to ignore ImportError in running tests
+# but for good coverage and completing testing method should define
+# correct values in local_variables and then run tests.
+# local_variables is ignored from staging in git.
+
+SAMPLE_ACCESS_TOKEN = 'H0xvNgoBH2y7bZw9jJWEmHhZDxU5F8alvSURoTJnffb8WJMmcpJpoPtjEnzCVvls'
+SAMPLE_ACCOUNT_ID = '8019940745056318'
+SAMPLE_MEDIA_ID = '2365457976114372'
