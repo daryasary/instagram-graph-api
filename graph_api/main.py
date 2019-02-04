552a89e23f92e06ac3d755066f533de608050062
Hosein <hs.ramezanpoor@gmail.com>
2019-02-04 17:07:44 +0330

    Add new handlers to the main InstagrmGRaphAPIHAndler

--- graph_api/main.py
+++ graph_api/main.py
@@ -12,16 +12,21 @@ class InstagramGraphHandler:
         account_tag=InstagramAccountTags,
         account_media_list=InstagramAccountMediaList,
         media_comments=InstagramMediaComments,
-        media_insights=InstagramMediaInsights
+        media_insights=InstagramMediaInsights,
+        hashtag_search=InstagramHashtagSearch,
+        hashtag_top_media=InstagramHashtagTopMedia,
+        hashtag_recent_media=InstagramHashtagRecentMedia
     )
 
-    def __init__(self, access_token, account_id=None, media_id=None):
+    def __init__(self, access_token, account_id=None, media_id=None, hashtag_id=None):
         self.access_token = access_token
         self.account_id = account_id
         self.media_id = media_id
+        self.hashtag_id = hashtag_id
         credentials = dict(
             access_token=self.access_token,
             instagram_media_id=self.media_id,
+            instagran_hashtag_id=self.hashtag_id,
             instagram_business_account_id=self.account_id
         )
         for attr in self.end_points.keys():
