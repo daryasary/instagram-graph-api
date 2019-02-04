8a3fdf0b1bfb15d198da9889ac0bfe747f8bc019
Hosein <hs.ramezanpoor@gmail.com>
2019-02-04 17:41:07 +0330

    Add renew method to the InstagramGraphHandler

--- graph_api/main.py
+++ graph_api/main.py
@@ -23,11 +23,32 @@ class InstagramGraphHandler:
         self.account_id = account_id
         self.media_id = media_id
         self.hashtag_id = hashtag_id
-        credentials = dict(
-            access_token=self.access_token,
-            instagram_media_id=self.media_id,
-            instagran_hashtag_id=self.hashtag_id,
-            instagram_business_account_id=self.account_id
+        credentials = self._build_credentials(
+            self.access_token, self.media_id, self.hashtag_id, self.account_id
         )
         for attr in self.end_points.keys():
             setattr(self, attr, self.end_points[attr](**credentials))
+
+    def _build_credentials(self, access_token=None, media_id=None,
+                           hashtag_id=None, account_id=None):
+        if access_token is None:
+            access_token = self.access_token
+        if media_id is None:
+            media_id = self.media_id
+        if hashtag_id is None:
+            hashtag_id = self.hashtag_id
+        if account_id is None:
+            account_id = self.account_id
+        credentials = dict(
+            access_token=access_token, instagram_business_account_id=account_id,
+            instagran_hashtag_id=hashtag_id, instagram_media_id=media_id,
+        )
+        return credentials
+
+    def renew(self, attrs, **kwargs):
+        if not isinstance(attrs, list):
+            attrs = [attrs]
+        credentials = self._build_credentials(**kwargs)
+        for attr in attrs:
+            if hasattr(self, attr):
+                setattr(self, attr, self.end_points[attr](**credentials))
